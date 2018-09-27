b97a6e2ddbd25c9e8896b0ff7d206b5e5c1a52ee
sarawuthjdp <41405574+sarawuthjdp@users.noreply.github.com>
2018-09-27 09:04:29 +0700

    Update server.js

--- server.js
+++ server.js
@@ -8,10 +8,8 @@ const PORT = process.env.PORT || 3000;
 const INDEX = path.join(__dirname, 'index.html');
 
 const server = express()
+  .use((req, res) => res.sendFile(INDEX) )
   .listen(PORT, () => console.log(`Listening on ${ PORT }`));
-  server.get('/', (req, res) => {
-     res.sendFile(INDEX)
-  });
 const wss = new SocketServer({ server });
 
 wss.on('connection', (ws) => {
